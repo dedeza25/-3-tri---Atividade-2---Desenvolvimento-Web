7fd89e34b0ef579001d63ce3ad3de7c60ce8011e
dedeza25 <andrezanascimento@colegiopaulodetarso.com.br>
2025-10-29 15:03:25 +0000

    3

---
+++ ex3.py
@@ -0,0 +1,8 @@
+inventario = [ "calça", "gloss", "blusa", "celular":]
+busca = "gloss"
+for item in inventario:
+    if item == busca;
+    print(f"(busca.capitalize()) foi encontrada !")
+    break
+else:
+    print("item não encontrado")
